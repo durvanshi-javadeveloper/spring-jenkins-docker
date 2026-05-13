4d55f20e60856c8c4401f6322b277c39406a137d
durvanshi-javadeveloper <mrghatage25@gmail.com>
2026-05-13 11:16:45 +0530

    first commit

---
+++ src/test/java/com/Spring_Jenkins_Docker/SpringJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_Jenkins_Docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
